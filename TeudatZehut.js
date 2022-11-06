1f5376e99bdea75bcd2618b7a78e56f37f22071b
Eugene Kuzenko <yevgenkuzenko@gmail.com>
2022-11-06 17:56:15 +0200

    TeudatZehut_version 2

--- TeudatZehut.js
+++ TeudatZehut.js
@@ -18,9 +18,6 @@
 
 
 console.log(" \nGenerated Teudat Zehut =>  ", checkTeudatZehut(generateRandomTeudatZehut()));
-console.log(" \nGenerated Teudat Zehut =>  ", checkTeudatZehut(generateRandomTeudatZehut()));
-console.log(" \nGenerated Teudat Zehut =>  ", checkTeudatZehut(generateRandomTeudatZehut()));
-
 console.log(checkTeudatZehut('123456782'));
 console.log(checkTeudatZehut('346784324'));
 console.log(checkTeudatZehut('12345a'));
@@ -28,84 +25,71 @@ console.log(checkTeudatZehut('123456789233'));
 console.log(checkTeudatZehut('1234'));
 
 function generateRandomTeudatZehut(){
-    let RandArr = [];
-    let NewTZ = [];
-    let StrNewTZ = "";
+    let randArr = [];
+    let newTZ = [];
+    let strNewTZ = "";
     for (let i = 0; i < 8; i++) {
-        RandArr[i] = Math.round(Math.random() * 9);
-        NewTZ[i] = RandArr[i];
+        randArr[i] = Math.round(Math.random() * 9);
+        newTZ[i] = randArr[i];
     }
-    RandArr =  ConvertEvenOdd(RandArr);
-    RandArr = sumElemBigNnum(RandArr);
-    RandArr = sumDigits(RandArr);
-    let checkDigit = 10 - RandArr % 10;
-    NewTZ[8] = checkDigit;
-    for (let i=0; i<NewTZ.length;i++){
-        StrNewTZ += NewTZ[i];
+    randArr =  convertEvenOdd(randArr);
+    randArr = sumElemBigNnum(randArr);
+    randArr = sumDigits(randArr);
+    let checkDigit = 10 - randArr % 10;
+    newTZ[8] = checkDigit;
+    for (let i=0; i<newTZ.length;i++){
+        strNewTZ += newTZ[i];
     }
-    return StrNewTZ;
+    return strNewTZ;
 }
 
 function checkTeudatZehut(teudatStrNumber) {
-    let Check = ChekInput(teudatStrNumber);
-    let arrIntNum = ConvertStr2ArrNum(Check);
-    let BigNum = ConvertEvenOdd(arrIntNum);
-    let processedNumber = sumElemBigNnum(BigNum);
+
+    let check = chekInput(teudatStrNumber);
+
+    let arrIntNum = convertStr2ArrNum(check);
+    let bigNum = convertEvenOdd(arrIntNum);
+    let processedNumber = sumElemBigNnum(bigNum);
     let res = sumDigits(processedNumber);
-    let validator = ValidatorTZ(res);
-    let PrintValid = prnValid(validator);
-return " \nNumber Teudat Zehut : "+teudatStrNumber+" : is "+PrintValid;
+    let validator = validatorTZ(res);
+    let printValid = prnValid(validator);
+return " \nNumber Teudat Zehut : "+teudatStrNumber+" : is "+printValid;
 }
 
-function ConvertStr2ArrNum(teudatStrNumber) {
-    let arTZ = Array.from(teudatStrNumber);
-    let arrTzNum = arTZ.map(function(number,index){
-                return arTZ[index] = +arTZ[index];
-    }); 
-    return  arrTzNum;
- }
-
-function ConvertEvenOdd(arIntNum) {
-        let intNum = arIntNum.map(function(number,index){
-        index % 2 != 0 ? number=number*2 : number;
-        return arIntNum[index] = number;
-     }); 
-return intNum;
+function convertStr2ArrNum(teudatStrNumber) {
+    return Array.from(teudatStrNumber).map(function(number){ return number = +number}); 
 }
 
-function sumElemBigNnum(BigNum) {
-        let Big = BigNum.map(function(num,index) {
-        num > 9 ? num = (num-10)+1 : num;
-        return BigNum[index] = num;
-    });
-return Big;
+function convertEvenOdd(arIntNum) {
+        return arIntNum.map(function(number,index){ index % 2 != 0 ? number=number*2 : number; return arIntNum[index] = number}); 
+}
+
+function sumElemBigNnum(bigNum) {
+        return bigNum.map(function(num,index) { num > 9 ? num = (num-10)+1 : num; return bigNum[index] = num });
 }
 
 function sumDigits(processedNumber) {
-        let res = processedNumber.reduce(function(result,number) {
-            return result + number;
-        }, 0);
-        return res;
+        return processedNumber.reduce(function(result,number) {return result + number;}, 0);
 }
 
-function ValidatorTZ(res) {
-    let TrueFalse = res % 10 == 0 ? true : false;
-    return TrueFalse;
+function validatorTZ(res) {
+    return res % 10 == 0 ? true : false;
 }
 
 function prnValid(validator) {
-    let valid = validator == true ? 'Valid' : 'Not Valid';
-    return valid;
+    return validator == true ? 'Valid' : 'Not Valid';
 }
 
-function ChekInput(teudatStrNumber) {
-    let arr = ConvertStr2ArrNum(teudatStrNumber);
+function chekInput(teudatStrNumber) {
+    let arr = convertStr2ArrNum(teudatStrNumber);
     for ( let i =0; i < arr.length; i++) {
         if ((arr[i] < 0 || arr[i] > 9) || (arr.length != 9)) {
         console.log("\nYour number : ", teudatStrNumber," : is NOT in the correct format.\n Please, enter number Teudat Zehut '9' digits from '0 - 9'\n");
         return process.exit(); 
+        //return false;
         } 
     }
     return teudatStrNumber;
+   //return true;
 }
 
